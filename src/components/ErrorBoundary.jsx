dc0f6617c2d288e01ef0894169240ac6cb352155
Ojima <akbenngold@gmail.com>
2024-04-22 10:49:11 +0100

    error boundary

--- src/components/ErrorBoundary.jsx
+++ src/components/ErrorBoundary.jsx
@@ -7,18 +7,24 @@ export default class ErrorBoundary extends Component {
 
     this.state = {
       hasError: false,
+      error: null,
     };
   }
 
   static getDerivedStateFromError(error) {
     return {
       hasError: true,
+      error: error,
     };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    console.error("Error caught by ErrorBoundary:", error, errorInfo);
   }
 
   render() {
     if (this.state.hasError) {
-      return <ErrorPage />;
+      return <ErrorPage error={this.state.error} />;
     }
 
     return this.props.children;
--- src/components/RepoDetails.jsx
+++ src/components/RepoDetails.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
+import ErrorBoundary from "./ErrorBoundary";
 
 function RepoDetails() {
   const location = useLocation();
@@ -10,29 +11,32 @@ function RepoDetails() {
   }
 
   return (
-    <div className="repodetails">
-      {console.log(repo)}
+    <ErrorBoundary>
+      {" "}
+      <div className="repodetails">
+        {console.log(repo)}
 
-      <h2>{repo.name}</h2>
-      <ul className="repolist">
-        {repo.description ? <li>Description: {repo.description}</li> : null}
-        {repo.language ? <li>Language: {repo.language}</li> : null}
-        <li>
-          Link to Github:{" "}
-          <a
-            href="http://github.com/akbenngold/altschool-assignments"
-            target="__blank"
-          >
-            {repo.html_url}
-          </a>
-        </li>
+        <h2>{repo.name}</h2>
+        <ul className="repolist">
+          {repo.description ? <li>Description: {repo.description}</li> : null}
+          {repo.language ? <li>Language: {repo.language}</li> : null}
+          <li>
+            Link to Github:{" "}
+            <a
+              href="http://github.com/akbenngold/altschool-assignments"
+              target="__blank"
+            >
+              {repo.html_url}
+            </a>
+          </li>
 
-        <li>Clone URL: {repo.clone_url}</li>
-        <li>Last Update: {repo.updated_at}</li>
-        <li>Date of Creation: {repo.created_at}</li>
-      </ul>
-      {console.log(repo)}
-    </div>
+          <li>Clone URL: {repo.clone_url}</li>
+          <li>Last Update: {repo.updated_at}</li>
+          <li>Date of Creation: {repo.created_at}</li>
+        </ul>
+        {console.log(repo)}
+      </div>
+    </ErrorBoundary>
   );
 }
 
